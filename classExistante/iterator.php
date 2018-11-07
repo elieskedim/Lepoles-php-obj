ba40193db4e6ea70c1b135a22552ddac59307039
elies <elieskedim23@gmail.com>
2018-11-07 13:02:21 +0100

    matin

---
+++ classExistante/iterator.php
@@ -0,0 +1,8 @@
+<?php
+//Pattern : Iterator
+//Pattern : Permet de proposer une solution commune pour resoudre des problème récurrents.
+//Iterator permet de parcourir des éléments de natures differentes
+
+$fuits = array('p' => 'pomme', 'f' => 'fraise', 'c' => 'cerise');
+
+$it1 = new ArrayIterator($fruits);
--- classExistante/stdClass.php
+++ classExistante/stdClass.php
@@ -1,2 +1,12 @@
 <?php
-$tab
+$tab = array('orange', 'fraise', 'poire');
+
+$objet = (object) $tab; // transformation (cast) 
+
+var_dump($objet);
+
+//Un objet fait parti de la stdClass (class standard de php) lorsque celui-ci est 'orphelin' et n'a pas été instancier par "new".
+
+$objet2 = new stdClass;
+$objet2->titre = 'PoleS';
+var_dump($objet2);
